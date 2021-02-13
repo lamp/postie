66ea1c864d3e1b7540a43e3e1c9461b2f3757f18
lamp <matthew.gradidge@gmail.com>
2021-02-13 14:24:10 -0500

    Initial Commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to postie
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/postie/core_test.clj
@@ -0,0 +1,7 @@
+(ns postie.core-test
+  (:require [clojure.test :refer :all]
+            [postie.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
