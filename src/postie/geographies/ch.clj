19474857f4c26ae8754c0450758204b1698f14a8
lamp <matthew.gradidge@gmail.com>
2021-12-24 12:26:22 -0500

    Implement CH postcode, partially test

--- src/postie/geographies/ch.clj
+++ src/postie/geographies/ch.clj
@@ -2,3 +2,19 @@
   (:require [malli.generator :as g]
             [postie.geographies.common :refer [preprocess postcode-patterns]]))
 
+(def pattern #"^[1-9][0-9]{3}$")
+
+(defn valid? [postcode]
+  (->> postcode
+      preprocess
+      (re-matches pattern)
+      some?))
+
+(defn format-postcode [postcode]
+  (let [sanitised-postcode (preprocess postcode)]
+    (if (valid? sanitised-postcode)
+      sanitised-postcode
+      false)))
+
+(def generator
+  (g/generator [:re pattern]))
--- test/postie/geographies/ch_test.clj
+++ test/postie/geographies/ch_test.clj
@@ -1,6 +1,21 @@
 (ns postie.geographies.ch-test
   (:require [clojure.test :refer [deftest testing is]]
-            [postie.geographies.ch :as sut]))
+            [postie.geographies.ch :as sut]
+            [malli.generator :as g]))
 
 (deftest valid?-test
-  (testing ""))
+  (testing "valid postcode"
+    (is (true? (sut/valid? "1204"))))
+  (testing "valid but badly formatted postcode"
+    (is (true? (sut/valid? "
+                           1204      "))))
+  (testing "an invalid postcode"
+    (is (false? (sut/valid? "ABCD")))))
+
+(deftest format-postcode-test
+  (testing "with a valid postcode"
+    (is (= "1204"
+           (sut/format-postcode "1204")))))
+
+(deftest generator-test
+  (is (sut/valid? (g/generate sut/generator))))
