165821e5f8093578d9a3a03a33a0b18b79da7040
lamp <matthew.gradidge@gmail.com>
2021-12-24 11:25:56 -0500

    Add ch and ch test

---
+++ src/postie/geographies/ch.clj
@@ -0,0 +1,4 @@
+(ns postie.geographies.ch
+  (:require [malli.generator :as g]
+            [postie.geographies.common :refer [preprocess postcode-patterns]]))
+
---
+++ test/postie/geographies/ch_test.clj
@@ -0,0 +1,7 @@
+(ns postie.geographies.ch-test
+  (:require [clojure.test :refer [deftest testing is]]
+            [postie.geographies.ch :as sut]))
+
+(deftest valid?-test
+  (testing ""))
+  
